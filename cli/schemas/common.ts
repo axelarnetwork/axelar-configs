976de7a83a376a035ae83e522705df0f10d346b8
Alan Soares <alanrsoars@gmail.com>
2023-08-24 16:34:55 +1200

    chore: add more chain configs

--- cli/schemas/common.ts
+++ cli/schemas/common.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const chainIconUrl = z
   .string()
-  .regex(/\/images\/chains\/([a-z]+)\.svg$/)
+  .regex(/\/images\/chains\/([a-z\-]+)\.svg$/)
   .describe(
     "the icon for the chain, must be a relative path to a svg icon under /images/chains"
   );
 
 export const currencyIconUrl = z
   .string()
-  .regex(/\/images\/tokens\/([a-z]+)\.svg$/)
+  .regex(/\/images\/tokens\/([a-z\-]+)\.svg$/)
   .describe(
     "the icon for the currency, must be a relative path to a svg icon under /images/tokens"
   );
