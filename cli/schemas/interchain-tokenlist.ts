5a0c9810c9255cf149a8e29de2d21e41795643a0
isaac markus <isaac@interoplabs.io>
2025-11-04 14:53:14 -0500

    feat: add new field to schema for interchain tokens

--- cli/schemas/interchain-tokenlist.ts
+++ cli/schemas/interchain-tokenlist.ts
@@ -28,6 +28,7 @@ export const interchainToken = z.object({
   chains: z.array(chains),
   coinGeckoId: z.string().optional(),
   deploymentMessageId: z.string(),
+  isDisabled: z.boolean().optional(),
 });
 
 export const version = z.object({
